a7caad96e5d46342e88bad90e79133f05ffc95a4
reckter <hannes@guedelhoefer.de>
2014-02-19 16:39:54 +0100

    commented code

--- src/de/tudarmstadt/gdi1/project/cipher/substitution/monoalphabetic/KeywordMonoalphabeticCipherImpl.java
+++ src/de/tudarmstadt/gdi1/project/cipher/substitution/monoalphabetic/KeywordMonoalphabeticCipherImpl.java
@@ -13,11 +13,11 @@ import java.util.ArrayList;
  */
 public class KeywordMonoalphabeticCipherImpl extends MonoalphabeticCipherImpl implements KeywordMonoalphabeticCipher {
 
-	/**
-	 *
-	 * @param password
-	 * @param alphabet
-	 */
+    /**
+     * Creates a new Keyword-Cipher
+     * @param password the word, that will be used for the beginning of the destination Alphabet
+     * @param alphabet the source alphabet
+     */
     public KeywordMonoalphabeticCipherImpl(String password, Alphabet alphabet) {
         super(alphabet, alphabet);
 
@@ -26,34 +26,33 @@ public class KeywordMonoalphabeticCipherImpl extends MonoalphabeticCipherImpl im
 
         ArrayList<Character> restAlphabet = new ArrayList<Character>();
 
+        // we delete all duplicates out of the word, so that passList only contains each letter once
         for(Character c: password.toCharArray()) {
             if(!passList.contains(c)) {
                 passList.add(c);
             }
         }
 
+        // we compute the rest of the alphabet, so each letter that isn't in passList, is in here
         for(Character c: sourceArray) {
             if(!passList.contains(c)) {
                 restAlphabet.add(c);
             }
         }
 
-        for(Character c: alphabet.asCharArray()) {
-            if(!passList.contains(c)) {
-                passList.add(c);
-            }
-        }
 
         Character[] destArray = new Character[sourceArray.length];
-
+        // putting the password into the destinationArray
         for(int i = 0; i < passList.size(); i++) {
             destArray[i] = passList.get(i);
         }
 
+        //putting the reveres Alphabet into the destination array
         for(int i = sourceArray.length - 1; i >= password.length(); i--) {
             destArray[i] = restAlphabet.get(sourceArray.length - i - 1);
         }
 
+        //finaly setting the destination to the right alphabet
         this.destination = new AlphabetImpl(destArray);
 
     }
